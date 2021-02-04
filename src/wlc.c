0c3b69ad078d84c5b14fa0fbb7839ba85f6dac75
channel-42 <59137829+channel-42@users.noreply.github.com>
2021-02-04 12:35:58 +0100

    Adding effect and palette change functions

--- src/wlc.c
+++ src/wlc.c
@@ -7,6 +7,22 @@
 
 #include "../include/config.h"
 
+// number of x - 1: used for loop exit conditions
+#define EFFECT_NUM 177
+#define PALETTE_NUM 55
+
+// make a curl http get request
+int req(CURL *curl, char *url) {
+  CURLcode resp;
+  curl_easy_setopt(curl, CURLOPT_URL, url);
+  resp = curl_easy_perform(curl);
+  // check request
+  if (resp != CURLE_OK) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 // make curl shut up
 size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
   return size * nmemb;
@@ -20,42 +36,86 @@ int chngBri(CURL *curl, uint8_t val) {
   sprintf(request, "%sA=%d", IP, val);
 
   // make request
-  curl_easy_setopt(curl, CURLOPT_URL, request);
-  resp = curl_easy_perform(curl);
-  curl_easy_cleanup(curl);
-
-  // check request
-  if (resp != CURLE_OK) {
-    exit(EXIT_FAILURE);
-    return 1;
-  } else {
-    return 0;
-  }
+  return req(curl, request);
 }
 
+int chngEff(CURL *curl, char *eff_name) {
+  CURLcode resp;
+  // request http url
+  char request[255];
+
+  // look if effect exists
+  uint8_t i = 0;
+  while (strcmp(effect_arr[i].name, eff_name)) {
+    if (i > EFFECT_NUM) {
+      return 1;
+    }
+    i++;
+  }
+  // request http url
+  sprintf(request, "%sFX=%d", IP, i);
+  // make request
+  return req(curl, request);
+}
+
+int chngPal(CURL *curl, char *pal_name) {
+  CURLcode resp;
+  // request http url
+  char request[255];
+
+  // look if palette exists
+  uint8_t i = 0;
+  while (strcmp(palette_arr[i].name, pal_name)) {
+    if (i > PALETTE_NUM) {
+      return 1;
+    }
+    i++;
+  }
+  // request http url
+  sprintf(request, "%sFP=%d", IP, i);
+  // make request
+  return req(curl, request);
+}
 int main(int argc, char *argv[]) {
 
-  CURL *curl;
-  curl = curl_easy_init();
+  CURL *curl = curl_easy_init();
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
 
   // check for if curl is OK
   if (curl) {
+    extern char *optarg;
     int opt;
     // parse through options
-    while ((opt = getopt(argc, argv, "o:bhsv")) != -1) {
+    while ((opt = getopt(argc, argv, "b:e:p:")) != -1) {
       switch (opt) {
-      case 'o':
+      case 'b':
         // change master brightness
         chngBri(curl, atoi(optarg));
         break;
+      case 'e':
+        // change master effect
+        if (chngEff(curl, optarg)) {
+          fprintf(stderr, "Unknown effect: %s\n", optarg);
+          break;
+        }
+        break;
+      case 'p':
+        // change master color palette
+        if (chngPal(curl, optarg)) {
+          fprintf(stderr, "Unknown color palette: %s\n", optarg);
+          break;
+        }
+        break;
       default:
         // default: print error
+        curl_easy_cleanup(curl);
         fprintf(stderr, "Usage: %s [-obhs] [value]\n", argv[0]);
         exit(EXIT_FAILURE);
         break;
       }
     }
   }
+  curl_global_cleanup();
+  curl_easy_cleanup(curl);
   return 0;
 }
